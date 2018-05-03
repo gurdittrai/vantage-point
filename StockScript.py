348a789768d23efca38ba727647d7831569e50fb
Gurditt Rai <raig@uoguelph.ca>
2018-05-02 20:43:50 -0400

    added plot

--- StockScript.py
+++ StockScript.py
@@ -15,21 +15,12 @@ else:
 endtime=data["Meta Data"]["3. Last Refreshed"]
 interval=data["Meta Data"]["4. Interval"]
 timeseries=data["Time Series (15min)"]
-
-print endtime
-print interval
-#print timeseries
-
 length=len(timeseries)
 
-error_config = {'ecolor': '0.3'}
-# starttime = timeseries.keys()[length-1]
-# print data
-# print endtime
-fig, ax = plt.subplots()
-bar_width=0.35
-opacity=0.4
-index=np.arange(length)
+# plt
+axes = plt.gca()
+axes.set_xlim([0,300])
+axes.set_ylim([90,100])
 
 # ["2018-05-02 16:00:00"]["2. high"]
 x = 0
@@ -37,10 +28,19 @@ high = "2. high"
 for timestamp in timeseries:
     x = x+1
     y = timeseries[timestamp][high]
-    print (str(timeseries[timestamp]) + '(' + str(x) + ',' + str(y) + ')')
+    print ('(' + str(x) + ',' + str(y) + ')')
+    plt.scatter(x, y)
 
+plt.show()
 
-
+# error_config = {'ecolor': '0.3'}
+# # starttime = timeseries.keys()[length-1]
+# # print data
+# # print endtime
+# fig, ax = plt.subplots()
+# bar_width=0.35
+# opacity=0.4
+# index=np.arange(length)
 # rects1 = ax.bar(index, timeseries[timestamp]["2. high"], bar_width,alpha=opacity, color='b', error_kw=error_config,label='MFST')
 # ax.set_xlabel('Date')
 # ax.set_ylabel('Price')
