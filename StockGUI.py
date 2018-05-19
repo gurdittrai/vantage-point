2f711e746553c7e92ac52e9bd8c6488ae9119dce
unknown <h3khaira@edu.uwaterloo.ca>
2018-05-19 15:36:11 -0400

    GUI initial commit

---
+++ StockGUI.py
@@ -0,0 +1,30 @@
+import tkinter as tk 
+LARGE_FONT=("Verdana",12)
+class stockapp(tk.Tk):
+    #initialize the app when the program is started 
+    def __init__(self,*args,**kwargs):
+        tk.Tk.__init__(self,*args,**kwargs)
+        #container should be the main window object
+        container=tk.Frame(self)
+        container.pack(side="top",fill="both",expand=True)
+        container.grid_rowconfigure(0,weight=1)
+        container.grid_columnconfigure(0,weight=1)
+
+        self.frames={}
+        #frame should represent different windows in the app
+        frame=StartPage(container,self)
+        self.frames[StartPage]=frame
+        frame.grid(row=0,column=0,sticky="nsew")
+        self.show_frame(StartPage)
+    def show_frame(self, cont):
+        #selecting the right window to display and displaying it
+        frame=self.frames[cont]
+        frame.tkraise()
+class StartPage(tk.Frame):
+    def __init__(self,parent, controler):
+            tk.Frame.__init__(self,parent)
+            label=tk.Label(self,text="Main Page",font=LARGE_FONT)
+            label.pack(pady=10,padx=10)
+
+app=stockapp()
+app.mainloop()
