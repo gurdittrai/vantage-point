d2c38fe9fa029df1f8819f93a1754e6ff9af78fc
unknown <h3khaira@edu.uwaterloo.ca>
2018-05-18 20:11:05 -0400

    fixed plotting issues. SMA is now plotting correctly

--- StockScript.py
+++ StockScript.py
@@ -1,11 +1,13 @@
 import datetime
+from dateutil.relativedelta import relativedelta
 import requests
 import json
 import matplotlib.pyplot as plt
 import numpy as np
 import config
 key=config.api_key
-
+#change this to get more/less data on plot
+interval=200
 stock="GOOGL"
 r=requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stock+'&outputsize=full&apikey='+key)
 #SMA is the simple moving average. The average of the 50 points surrounding the point of interest
@@ -31,31 +33,30 @@ starttime=sorted(timeseries.keys())[0]
 SMAtechnical=SMAdata["Technical Analysis: SMA"]
 SMAendtime=SMAdata["Meta Data"]["3: Last Refreshed"]
 SMAstarttime=sorted(SMAtechnical.keys())[0]
-print (SMAendtime)
-print (SMAendtime[5:7])
 startdate = datetime.date(int(starttime[0:4]),int(starttime[5:7]), int(starttime[8:10]) )
 enddate = datetime.date(int(endtime[0:4]),int(endtime[5:7]), int(endtime[8:10]) )
 SMAstartdate = datetime.date(int(SMAstarttime[0:4]),int(SMAstarttime[5:7]), int(SMAstarttime[8:10]) )
 SMAenddate = datetime.date(int(SMAendtime[0:4]),int(SMAendtime[5:7]), int(SMAendtime[8:10]) )
+#delta calculates the number of days between two inputs output format: 5020 days, 0:00:00
+#we want to limit the amount of data we plot to avoid crashing the program 
+SMAintervaldate=SMAenddate + relativedelta(days=-interval)
+SMAbreakdate=SMAintervaldate.strftime('%Y-%m-%d')
 #length=len(SMAtechnical)
 xticks=[]
 SMAvalue=[]
-limiter=0
-xticksnum=[]
 fig=plt.figure(facecolor='white')
 ax=plt.axes()
 for date in SMAtechnical:
     xticks.append(date)
-    xticksnum.append(limiter)
-    SMAvalue.append(SMAtechnical[date]["SMA"])
-    limiter=limiter+1
-    plt.scatter(limiter,SMAtechnical[date]["SMA"])
-    if limiter>100:
+    SMAvalue.append(float(SMAtechnical[date]["SMA"]))
+    #plt.scatter(limiter,float(SMAtechnical[date]["SMA"]))
+    #stops reading the data once interval end is reached
+    if date==SMAbreakdate:
         break
 # plt
 #axes = plt.gca()
 ax.set_xlabel("Time")
-ax.set_ylabel("Price (Highs)")
+ax.set_ylabel("Simple Moving Average (USD)")
 
 # ["2018-05-02 16:00:00"]["2. high"]
 yhigh, ylow = -1, -1
@@ -65,11 +66,9 @@ for timestamp in timeseries:
     # get values
     x = x+1
     y = float(timeseries[timestamp][high])
-
     # limit output
     if (x > 100):
         break
-
     # get range
     if (yhigh == -1):
         yhigh = y
@@ -81,15 +80,20 @@ for timestamp in timeseries:
 
     # print values to terminal
     #print ('(' + str(x) + ',' + str(y) + ')')
-    plt.scatter(x, y)
-    
+    #plt.scatter(x, y)   
 font = {'family': 'serif',
         'color':  'darkred',
         'weight': 'normal',
         'size': 16,
         }
-plt.text(0.75, 0.9, 'SMA/Daily High', transform=axes.transAxes)
-ax.set_xlim(xmin=0,xmax=100)
-ax.set_ylim(ymin=ylow,ymax=yhigh)
-
+#reversing the lists since they are read from most recent time to the interval end
+plotxticks=xticks[::-1]
+plotSMAvalue=SMAvalue[::-1]
+SMAline = plt.plot(plotxticks,plotSMAvalue,lw=2.5,color='#1f77b4')
+start, end = ax.get_xlim()
+ax.xaxis.set_ticks(np.arange(start, end, interval//10))
+fig.autofmt_xdate(bottom=0.2, rotation=30, ha='right')
+plt.text(0.9, 0.9, stock, transform=ax.transAxes)
+#ax.set_xlim(xmin=0,xmax=100)
+#ax.set_ylim(ymin=ylow,ymax=yhigh)
 plt.show()
