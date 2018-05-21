9ca6738ef9c846ab375325ed612b8cd85d47e581
Gurditt Rai <raig@uoguelph.ca>
2018-05-21 01:06:29 -0400

    added exit btn and testing better way of doing start page with test page

--- StockGUI.py
+++ StockGUI.py
@@ -28,37 +28,42 @@ class stockapp(tk.Tk):
 
         self.frames={}
         #add the pages in this list everytime you make a new page
-        for F in (StartPage,PlotPage):
+        for F in (StartPage,PlotPage,TestPage):
             #frame should represent different windows in the app
             frame=F(container,self)
             self.frames[F]=frame
             frame.grid(row=0,column=0,sticky="nsew")
+        
         self.show_frame(StartPage)
+
     def show_frame(self, cont):
         #selecting the right window to display and displaying it
         frame=self.frames[cont]
         frame.tkraise()
 #creates a window "StartPage"
 class StartPage(tk.Frame):
+
     def __init__(self,parent, controller):
         root = tk.Frame.__init__(self,parent)
         label = tk.Label(self,text="Main Page",font=LARGE_FONT)
-        
-        fields = ('Stock Symbol', 'Interval (days)')
 
-        stock_symbol_entry=Entry(self)
-        user_input=tk.Label(self, text="Stock Symbol",font=small_font)
+        #create field
+        stock_symbol_entry = Entry(self)
+        user_input = tk.Label(self, text="Stock Symbol",font=small_font)
         
-        interval_entry=Entry(self)
-        interval_input=tk.Label(self, text="Interval (days)",font=small_font)
+        interval_entry = Entry(self)
+        interval_input = tk.Label(self, text="Interval (days)",font=small_font)
 
+        #set default values on the field
         stock_symbol_entry.insert(10,'GOOGL')
         interval_entry.insert(10,7)
         
-        button1=ttk.Button(self,text="Plot Page",command=lambda: controller.show_frame(PlotPage))
-        #button2=ttk.Button(self,text="Enter1",command=get_interval())
-        button3=ttk.Button(self,text="Enter2",command=lambda: print("Symbol: %s\nInterval: %s" % (stock_symbol_entry.get(), interval_entry.get())))
-        
+        btn_stock = ttk.Button(self,text="Enter1")
+        btn_interval = ttk.Button(self,text="Enter2",command=lambda: print("Symbol: %s\nInterval: %s" % (stock_symbol_entry.get(), interval_entry.get())))
+        btn_plot = ttk.Button(self,text="Plot Page",command=lambda: controller.show_frame(PlotPage))
+        btn_test = ttk.Button(self,text="Test Page",command=lambda: controller.show_frame(TestPage))
+        btn_exit = ttk.Button(self,text="Exit",command=lambda: exit(1))
+
         #grid
         label.grid(row=0,column=0)
 
@@ -68,9 +73,11 @@ class StartPage(tk.Frame):
         interval_input.grid(row=2,column=0)
         interval_entry.grid(row=2,column=1)
 
-        button1.grid(row=3,column=0)
-        #button2.grid(row=1,column=3)
-        button3.grid(row=2,column=3)
+        btn_stock.grid(row=1,column=3)
+        btn_interval.grid(row=2,column=3)
+        btn_plot.grid(row=3,column=0)
+        btn_test.grid(row=3,column=1)
+        btn_exit.grid(row=4,column=0)
 
 #we can copy this (almost) exactly to create more pages
 class PlotPage(tk.Frame):
@@ -89,9 +96,51 @@ class PlotPage(tk.Frame):
         toolbar=NavigationToolbar2TkAgg(canvas,self)
         toolbar.update()
         canvas._tkcanvas.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
-def get_interval():
-    print("hello")
-    #interval=tk.IntVar()
-    #print ("Test: "+str(interval.get()))
+
+class TestPage(tk.Frame):
+    #print values of fields
+    def fetch(entries):
+        for entry in entries:
+            field = entry[0]
+            text = entry[1].get()
+            print('%s: %s' % (field, text))
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label=tk.Label(self,text="Test Page",font=LARGE_FONT)
+        btn_home = ttk.Button(self,text="Home Page",command=lambda: controller.show_frame(StartPage))
+
+        #create input fields
+        fields = 'Stock Symbol', 'Interval (days)'
+        field_defaults = 'GOOGL', '7'
+        entries = []
+        for field, fdefault in zip(fields, field_defaults):
+            #three columns
+            rowlabel = tk.Label(self, text=field, font=small_font)
+            entry = Entry(self)
+            entry.insert(10,fdefault)
+            btn = ttk.Button(self,text="Enter",command=lambda: print('%s: %s' % (field, entry.get())))
+            #add to list
+            entries.append((rowlabel, entry, btn, field))
+
+        #grid
+        rowCount = 0
+
+        #adding start and end rows
+        label.grid(row=rowCount,column=0)
+        rowCount += 1
+        
+        #adding entries to the grid
+        for r, entry in enumerate(entries, start=rowCount):
+            for c in range(0,3):
+                entry[c].grid(row=r,column=c)
+            rowCount += 1
+
+        btn_home.grid(row=rowCount,column=0)
+
+#btn function
+    def showValue(field, entry):
+        print('%s: %s' % (field, entry.get()))
+
 app=stockapp()
 app.mainloop()
